a6635eb20687d16ae89da0662fbe84b7042bb417
Peachying <1120025860@qq.com>
2018-03-26 10:48:09 +0800

    fix bug

--- src/com/sg/controller/Shipwork_historyController.java
+++ src/com/sg/controller/Shipwork_historyController.java
@@ -1,60 +1,93 @@
 /**
  * 
  */
-package com.sg.domain;
+package com.sg.controller;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sg.domain.Ship;
+import com.sg.domain.Shipwork_history;
+
+import net.sf.json.JSONObject;
 
 /**
  * @author yuchang xu
  *
  * 2018-03-20
  */
-public class Shipwork_history {
-	private int mmsi;
-	private int company_id;
-	private String startdate;
-	private String enddate;
-	private int route_id;
-	private int project_id;
-	public int getMmsi() {
-		return mmsi;
-	}
-	public void setMmsi(int mmsi) {
-		this.mmsi = mmsi;
-	}
-	public int getCompany_id() {
-		return company_id;
-	}
-	public void setCompany_id(int company_id) {
-		this.company_id = company_id;
-	}
-	public String getStartdate() {
-		return startdate;
-	}
-	public void setStartdate(String startdate) {
-		this.startdate = startdate;
-	}
-	public String getEnddate() {
-		return enddate;
-	}
-	public void setEnddate(String enddate) {
-		this.enddate = enddate;
-	}
-	public int getRoute_id() {
-		return route_id;
-	}
-	public void setRoute_id(int route_id) {
-		this.route_id = route_id;
-	}
-	public int getProject_id() {
-		return project_id;
-	}
-	public void setProject_id(int project_id) {
-		this.project_id = project_id;
-	}
-	@Override
-	public String toString() {
-		return "Shipwork_history [mmsi=" + mmsi + ", company_id=" + company_id + ", startdate=" + startdate
-				+ ", enddate=" + enddate + ", route_id=" + route_id + ", project_id=" + project_id + "]";
+@Controller
+@RequestMapping("/shipworkhistory")
+public class Shipwork_historyController {
+	public SqlSession getSession() throws IOException{
+		String resource = "mybatis-config.xml";
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session=sqlSessionFactory.openSession();
+        return session;
 	}
 	
+	@RequestMapping(value="/add",method=RequestMethod.POST)
+	 @ResponseBody
+	public ResponseEntity<String> add(@RequestBody String pro) throws IOException{
+		System.out.println("插入数据");
+		System.out.println(pro);
+		JSONObject json = JSONObject.fromObject(pro);
+		Shipwork_history shiphis = new Shipwork_history();
+		shiphis.setMmsi(json.getInt("mmsi"));
+		shiphis.setCompany_id(json.getInt("company_id"));
+		shiphis.setRoute_id(json.getInt("route_id"));
+		shiphis.setStartdate(json.getString("startdate"));
+		shiphis.setEnddate(json.getString("enddate"));
+		shiphis.setProject_id(json.getInt("project_id"));
+		SqlSession session = this.getSession();
+		session.insert("addShipworkhistory",shiphis);
+		session.commit();
+		session.close();
+		String str ="success!!!!";
+	    return new ResponseEntity<String>(str, HttpStatus.OK);
+	}
+	@RequestMapping(value="/delete",method=RequestMethod.POST)
+	 @ResponseBody
+	public ResponseEntity<String> delete(@RequestBody String pro) throws IOException{
+		System.out.println("删除数据");
+		System.out.println(pro);
+		JSONObject json = JSONObject.fromObject(pro);
+		Ship ship = new Ship();
+		ship.setMmsi(json.getInt("mmsi"));
+		ship.setFleet_id(json.getInt("company_id"));
+		ship.setRoute_id(json.getInt("route_id"));
+		ship.setStartdate(json.getString(json.getString("startdate")));
+		ship.setEnddate(json.getString("enddate"));
+		ship.setProject_id(json.getInt("project_id"));
+		SqlSession session = this.getSession();
+		session.delete("deleteShipworkhis",ship);
+		session.commit();
+		session.close();
+		String str ="success!!!!";
+	    return new ResponseEntity<String>(str, HttpStatus.OK);
+	}
+	@RequestMapping(value="/listbymmsi",method=RequestMethod.POST)
+	@ResponseBody
+	public ResponseEntity<List<Shipwork_history>> listbymmsi(@RequestBody String pro) throws IOException{
+		JSONObject json = JSONObject.fromObject(pro);
+		int mmsi=json.getInt("mmsi");
+		int project_id = json.getInt("project_id");
+		SqlSession session = this.getSession();
+		List<Shipwork_history> res = session.selectList("getShipworkhisbyid",mmsi);
+		return new ResponseEntity<List<Shipwork_history>>(res,HttpStatus.OK);
+	}
 }

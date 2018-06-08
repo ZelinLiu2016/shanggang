7a4401f242a841818158549467fa20b3b56d6e73
zelinliu <842376777@qq.com>
2018-06-08 18:18:42 +0800

    updat ui

--- WebContent/htmls/static/assets/js/user_login.js
+++ WebContent/htmls/static/assets/js/user_login.js
@@ -1,25 +1,28 @@
-function system2_login(thisform) {
-	pwd = password.value;
-	password.value = SHA1(password.value);
+function system2_login() {
+	username = $("#userId").val();
+	pwd = $("#password").val();
+	md5pwd = SHA1(password.value);
+	var login_data = {"username": username, "password": md5pwd};
 	$.ajax({
-		url: "http://202.120.38.3:8091/login",
-		type: "GET",
-		data: {userId: userId.value, password: password.value}
-	}).fail(function() {
-		// $('#loadingModal').modal('hide');
-		alert("未连接到服务器!");
-	}).done(function(data) {
-		// $('#loadingModal').modal('hide');
-		//changeInfoDialog(data);
-
-		if(data.isSuccess==1){
-			data["pwd"] = pwd;
-			system2_setSession(data);
-
-			self.location='MapView_new.htm';
-		}else{
-			alert("用户名或密码错误！")
-		}
+		url: "/shanggang/user_sys2s/verification",
+		type: "POST",
+		data: JSON.stringify(login_data),
+		contentType:"application/json",
+		success: function (data) {
+			console.log(data);
+        	if(data.isSuccess==1)
+        	{
+				data["pwd"] = pwd;
+				system2_setSession(data);
+				self.location='MapView_new.htm';
+			}
+			else{
+				alert("用户名或密码错误！");
+			}   
+		},
+        error: function () {       
+                alert("未连接到服务器!");
+        }
 	});	
 }
 
@@ -53,7 +56,7 @@ function system2_setSession(data) {
 		sessionStorage.userId = data.userId;
         sessionStorage.name = data.name;
 		sessionStorage.phone = data.phone;
-		sessionStorage.privilege = data.privilege;
+		sessionStorage.privilege = "WW";
 		sessionStorage.unit = data.unit;
 		sessionStorage.warningStatus = data.warningStatus;
 		sessionStorage.pwd = data.pwd;
